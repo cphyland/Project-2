a7114744383c2dddb7edee1460a03a08f8e0ac2f
bakerv <80018640+bakerv@users.noreply.github.com>
2021-07-27 15:25:00 -0500

    cleaned specieces and report owner columns

--- js/shipwreckdata.js
+++ js/shipwreckdata.js
@@ -1,7 +1,33 @@
 shipDataRaw = "../data/Shipwrecks/ShipwrecksWAM_002.geojson"
 
+var overlayMaps = {
+    Ships: new L.LayerGroup(),
+   
+};
+
+var map = L.map("map", {
+    center: [37.09,-95.71],
+    zoom: 5,
+    worldCopyJump: "True",
+    layers: [
+        overlayMaps.Ships
+   
+    ]
+});
+
+var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+tileSize: 512,
+maxZoom: 18,
+zoomOffset: -1,
+id: "mapbox/streets-v11",
+accessToken: API_KEY
+}).addTo(map);
+
 let data = d3.json(shipDataRaw).then((rawData)=> {
-    console.log(rawData);
+    var ships = L.geoJSON(rawData.features);
+    ships.addTo(map);
+    ships.addTo(overlayMaps['Ships'])
 });
 
 console.log("test linkage")
