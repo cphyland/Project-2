a984652c3348207f5701ad466dcdd54110313245
bakerv <80018640+bakerv@users.noreply.github.com>
2021-07-29 13:36:11 -0500

    transfered sharkdata functions and sqlalchemy functions

---
+++ js/flask_functions.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import requests
+from sqlalchemy import create_engine
+
+
+# paths to access and save data
+shark_url= "http://api.fish.wa.gov.au/webapi/v1/RawData"
+
+def shark_data(api_url, start_date, end_date):
+    """
+        Access the API at the specified URL, clean the data, filter the data, and return the JSON object as a pandas DataFrame
+            Args:
+                api_url (str): the URL to download the desired data from the API
+                start_date (str): return data beginning at this date YYYY-MM-DD
+                end_date (str): return data ending at this date YYYY-MM-DD
+    """
+
+    # get a JSON object from the specified url, and convert it to a pandas dataframe
+    def get_sharks(api_url):
+    
+        response = requests.get(api_url).json()
+        sharks_df = pd.DataFrame(response)
+
+        return sharks_df
+
+    # clean the rawdata returned from the API to correct duplicate entries, and remove reports that are not sharks
+    def clean_sharks(sharks_df):
+        clean_sharks = sharks_df.copy()
+
+        # correct duplicate species entries present in the data
+        clean_sharks['SightingSpeciesValue'] = clean_sharks['SightingSpeciesValue'].replace({
+        'Whale Carcass':'whale carcass',
+        'whale carcass ':'whale carcass',
+        'whaler':'bronze whaler',
+        'shortfin mako':'mako', 
+        'white ': 'white'})
+
+        # correct duplicate report owner entries present in the data
+        clean_sharks['OwnerValue'] = clean_sharks['OwnerValue'].replace({
+        'Fisheries advise': 'Fisheries Advise',
+        'SLS Lifesavers report ': 'SLS Lifesavers report',
+        'SLS Westpac Heli': 'SLS Westpac Heli report',
+        'Public Report': 'Public report'})
+
+        # drop entries that do not correspond to a confirmed shark sighting
+        drop_species = ['whale carcass',   # create a list of species values to filter out
+                        'detection event - possible',
+                        'other',
+                        'sonar object'] 
+        species_filter = ~clean_sharks.SightingSpeciesValue.isin(drop_species) # use the list to create a filter
+        clean_sharks = clean_sharks[species_filter] # apply the filter to the dataframe
+
+        # limit the data to what is required by the leaflet plot
+        clean_sharks = clean_sharks[['InteractionValue','InteractionId','SightingSpeciesValue','SightingDateTime','OwnerValue','LocationX','LocationY']]
+
+        return clean_sharks
+
+    # limit the data to entries in a specified interval
+    def date_filter(data,start_date,end_date):
+        filtered_df = data.copy()
+        filtered_df = filtered_df.loc[
+        (clean_sharks["SightingDateTime"] > start_date) & (clean_sharks["SightingDateTime"] < end_date)]
+
+        return filtered_df
+
+    rawdata = get_sharks(api_url)
+    clean_data = clean_sharks(rawdata)
+    filtered_df = date_filter(clean_data,start_date,end_date)
+
+    return filtered_df
+
+def load_database(data_df,table_name,connection_string):
+    engine = create_engine(f'sqlite://{connection_string}')
+    data_df.to_sql(table_name, con = engine)
+    
+
+
+
+
+
+
+
+
