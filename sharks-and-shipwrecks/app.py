89fbeea2da194a98a305b11f7757b6256677b6af
bakerv <80018640+bakerv@users.noreply.github.com>
2021-07-30 14:29:21 -0500

    debugging flask app after updating folder structure

--- sharks-and-shipwrecks/app.py
+++ sharks-and-shipwrecks/app.py
@@ -3,16 +3,17 @@ import flask_functions
 from sqlalchemy import create_engine
 import pandas as pd
 
-shark_api_url = "http://api.fish.wa.gov.au/webapi/v1/RawData"
-shark_table_name = "sharks1"
-shark_csv_path = "../data/sharks/sharks_cleaned.csv"
+
 
 
 #################################################
 # Database Setup
 #################################################
 #engine = create_engine("sqlite:///data/sharks.sqlite")
-
+shark_api_url = "http://api.fish.wa.gov.au/webapi/v1/RawData"
+shark_table_name = "sharks"
+shark_csv_path = "static/data/sharks/sharks_cleaned.csv"
+sqlite_path = "static/data/sharks/sharks-and-ships.sqlite"
 
 #################################################
 # Flask Setup
@@ -30,11 +31,14 @@ def index():
 
 @app.route("/update")
 def update_data():
+    # access, clean, and save the data to a sqlite database
     sharks_df = flask_functions.shark_data(shark_api_url, "2020-01-01", "2021-07-01")
-    flask_functions.load_database(sharks_df, shark_table_name, "data/sharks/australia.sqlite")
+    flask_functions.load_database(sharks_df, shark_table_name, sqlite_path)
 
-    sharks_df.to_csv("data/sharks/sharks_cleaned.csv")
+    # save the data to a csv file for the leaflet app
+    sharks_df.to_csv(shark_csv_path)
 
+    # send user to the main page on task completion
     return redirect("/")
 
 if __name__ == "__main__":
