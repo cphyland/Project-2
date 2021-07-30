c26c2a66b64e3b389dcab471951de4ec0a50720a
cphyland <court.hyland@gmail.com>
2021-07-29 21:02:32 -0500

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, jsonify
-import datetime as dt
-from types import FunctionType
 import numpy as np
 
 import sqlalchemy
@@ -9,7 +7,42 @@ from sqlalchemy.orm import Session, session
 from sqlalchemy import create_engine, inspect
 from sqlalchemy import func
 
+shark_url = "http://api.fish.wa.gov.au/webapi/vi.RawData"
+
+
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///data/sharks.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+#Base.classes.keys()
+# Save reference to the table
+Sharks = Base.classes.sharks
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+    shark_data(shark_url, "1/1/2020", "7/1/2021")
+    
+    )
+
+
+@app.route("/")
+    cleandata = shark_data(shark_url, "1/1/2020", "7/1/2021")
+    load_database(cleandata, "cleanSharkTable", "data/projectdatabase")
+
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
