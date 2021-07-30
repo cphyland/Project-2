57ee75cadd9d8be741320ec56e33043dcf925ae3
cphyland <court.hyland@gmail.com>
2021-07-29 21:42:43 -0500

    app push

--- js/app.py
+++ js/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, jsonify
 import numpy as np
-
+import flask_functions
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session, session
 from sqlalchemy import create_engine, inspect
 from sqlalchemy import func
 
-shark_url = "http://api.fish.wa.gov.au/webapi/vi.RawData"
+shark_url = "http://api.fish.wa.gov.au/webapi/v1.RawData"
 
 
 #################################################
@@ -15,14 +15,6 @@ shark_url = "http://api.fish.wa.gov.au/webapi/vi.RawData"
 #################################################
 engine = create_engine("sqlite:///data/sharks.sqlite")
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-#Base.classes.keys()
-# Save reference to the table
-Sharks = Base.classes.sharks
-session = Session(engine)
 
 #################################################
 # Flask Setup
@@ -32,17 +24,13 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
 @app.route("/")
-    shark_data(shark_url, "1/1/2020", "7/1/2021")
+def index():
+    cleandata = flask_functions.shark_data(shark_url, "1/1/2020", "7/1/2021")
+    flask_functions.load_database(cleandata, "cleanSharkTable", "data/projectdatabase")
     
-    )
 
-
-@app.route("/")
-    cleandata = shark_data(shark_url, "1/1/2020", "7/1/2021")
-    load_database(cleandata, "cleanSharkTable", "data/projectdatabase")
-
-    )
 
 if __name__ == "__main__":
     app.run(debug=True)
